e9dcd931580b1341e15dbe666cae7b832efc5c8b
ashersuman <user11root@gmail.com>
2021-09-23 18:25:19 +0530

    add address in enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   contactNumberSales: "+91 8825186895",
   contactNumberTechnical: "+91 8210707176",
   email: "ecuisine.in@gmail.com",
-  address: "",
+  address: "Bihar",
   sheetURL: "https://script.google.com/macros/s/AKfycbzwaWuV5Uq1QiXkXsba5hnRTJUTcIyJIb9xCTO1rmbcNYLfR-cy/exec",
 };
